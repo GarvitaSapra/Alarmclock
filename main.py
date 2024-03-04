1981c54154491f8e8d8865982a1d0f63194cc90f
Garvita Sapra <86355185+GarvitaSapra@users.noreply.github.com>
2024-03-04 17:02:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from datetime import datetime
-from playsound import playsound
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
+import winsound
+
+alarm_time = input("Enter the time of alarm to be set (HH:MM:SS AM/PM):\n")
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
 alarm_period = alarm_time[9:11].upper()
 print("Setting up alarm..")
+
 while True:
     now = datetime.now()
     current_hour = now.strftime("%I")
     current_minute = now.strftime("%M")
     current_seconds = now.strftime("%S")
     current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
+    
+    if alarm_period == current_period:
+        if alarm_hour == current_hour:
+            if alarm_minute == current_minute:
+                if alarm_seconds == current_seconds:
                     print("Wake Up!")
-                    playsound('audio.mp3')
+                    try:
+                        frequency = 2500  # Set frequency to 2500 Hertz
+                        duration = 1000  # Set duration to 1000 milliseconds (1 second)
+                        winsound.Beep(frequency, duration)
+                    except ValueError:
+                        print("Error: Beep sound cannot be played.")
                     break
